496f1f395f7e4d56b66c334876a0cfe504c24831
whoisYoges <helloyogesh@tuta.io>
2022-06-22 19:24:38 +0545

    Added scratchpad for instant notes + made dwm dey binding wm independent + changed colorscheme

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 4;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
@@ -10,16 +10,18 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 12;       /* vertical padding of bar */
 static const int sidepad            = 12;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Comic Sans MS:size=12" };
-static const char dmenufont[]       = "Comic Sans MS:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#20a024";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#000000";
+
+static const char normbg[]			= "#222222";
+static const char normfg[]			= "#78fa6e";
+static const char normborder[]      = "#bbc700";
+static const char selbg[]			= "#222222";
+static const char selfg[]			= "#db9f00";
+static const char selborder[]		= "#93ea45";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg			bg			border   */
+	[SchemeNorm] = { normfg,	normbg,		normborder},
+	[SchemeSel]  = { selfg,		selbg,		selborder  },
 };
 
 typedef struct {
@@ -31,6 +33,7 @@ const char *spcmd2[] = {"alacritty", "--class", "spterm2", "--config-file", "/ho
 const char *spcmd3[] = {"alacritty", "--class", "spterm3", "--config-file", "/home/useless/.config/alacritty/alacritty-scratchpad.yml", NULL };
 const char *spcmd4[] = {"keepassxc", NULL }; //password manager
 const char *spcmd5[] = {"deadbeef", NULL }; //music player
+const char *spcmd6[] = {"geany", NULL }; //todo and instant notes
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm1",     spcmd1},
@@ -38,6 +41,7 @@ static Sp scratchpads[] = {
 	{"spterm3",     spcmd3},
     {"keepassxc",   spcmd4},
     {"deadbeef",    spcmd5},
+    {"geany",		spcmd6},
 };
 
 /* tagging */
@@ -57,6 +61,7 @@ static const Rule rules[] = {
 	{ NULL,			"spterm3",	NULL,		   	SPTAG(2),  1,		   0, 			0,		  -1 },
     { NULL,         "keepassxc", NULL,          SPTAG(3),  1,          0,           0,        -1 },
     { NULL,         "deadbeef", NULL,           SPTAG(4),  1,          0,           0,        -1 },
+    { NULL,         "geany",	NULL,           SPTAG(5),  1,          0,           0,        -1 },
 };
 
 /* layout(s) */
@@ -84,12 +89,9 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -127,20 +129,15 @@ static Key keys[] = {
 	{ MODKEY,            			XK_v,  	   togglescratch,  {.ui = 2 } },
     { Mod4Mask,                     XK_p,      togglescratch,  {.ui = 3 } },
     { Mod4Mask,                     XK_a,      togglescratch,  {.ui = 4 } },
+    { Mod4Mask,                     XK_t,      togglescratch,  {.ui = 5 } },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-/*	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} }, */
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
-/*	{ ClkTagBar,            0,              Button1,        view,           {0} },
-	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },*/
 };
 
